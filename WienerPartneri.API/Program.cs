c408d48266bc394d8c1d1ff028d7ab0a359503d6
AnamariaMarjanovic <marjanovic.anamaria@outlook.com>
2026-06-21 19:16:23 +0200

    feat: add CORS policy to allow Angular frontend

--- WienerPartneri.API/Program.cs
+++ WienerPartneri.API/Program.cs
@@ -7,6 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+// CORS za Angular
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Registracija repositorija
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 builder.Services.AddSingleton<IPartnerRepository>(_ => new PartnerRepository(connectionString));
@@ -18,6 +29,7 @@ var app = builder.Build();
 var dbInitializer = new DatabaseInitializer(connectionString);
 dbInitializer.Initialize();
 
+app.UseCors("AllowAngular");
 app.UseAuthorization();
 app.MapControllers();
 
