6a97352c6758d3498f5bc08fae887b6cd7a36536
AnamariaMarjanovic <marjanovic.anamaria@outlook.com>
2026-06-21 19:00:03 +0200

    feat: configure app routing

--- wiener-partneri-FE/src/app/app.routes.ts
+++ wiener-partneri-FE/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PartnerList } from './components/partner-list/partner-list';
+import { PartnerForm } from './components/partner-form/partner-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PartnerList },
+  { path: 'partner/new', component: PartnerForm },
+  { path: '**', redirectTo: '' },
+];
